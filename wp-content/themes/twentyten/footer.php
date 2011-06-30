003b4dded6037c3d1e240678de00c447344246e7
james-mcmr <james@mccormackmorrison.com>
2011-07-01 00:34:02 +0100

    Meaningless change to footer

--- wp-content/themes/twentyten/footer.php
+++ wp-content/themes/twentyten/footer.php
@@ -23,7 +23,8 @@
 ?>
 
 			<div id="site-info">
-				<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				Test
+                <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 					<?php bloginfo( 'name' ); ?>
 				</a>
 			</div><!-- #site-info -->
