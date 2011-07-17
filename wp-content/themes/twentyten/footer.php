2193a555f599bd3b65be641cc591d2bcbee44ceb
james-mcmr <james@mccormackmorrison.com>
2011-07-17 15:43:13 +0100

    Aligned project boxes to left hand side. Also made all margins 10px

--- wp-content/themes/twentyten/footer.php
+++ wp-content/themes/twentyten/footer.php
@@ -23,7 +23,6 @@
 ?>
 
 			<div id="site-info">
-				Test
                 <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 					<?php bloginfo( 'name' ); ?>
 				</a>
