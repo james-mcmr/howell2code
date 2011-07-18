57d60ee0a119a7824c3415605728207da17ff02a
james-mcmr <james@mccormackmorrison.com>
2011-07-18 21:55:56 +0100

    Added pagination to Project Detail page. Can only paginate in current category.

--- wp-content/themes/twentyten/loop-single.php
+++ wp-content/themes/twentyten/loop-single.php
@@ -18,8 +18,8 @@
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 				<div id="nav-above" class="navigation">
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
+					<div class="nav-previous"><?php previous_post_link('%link', '%title', TRUE); ?> </div>
+					<div class="nav-next"><?php next_post_link('%link', '%title', TRUE); ?> </div>
 				</div><!-- #nav-above -->
 
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -59,8 +59,8 @@
 				</div><!-- #post-## -->
 
 				<div id="nav-below" class="navigation">
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
+					<div class="nav-previous"><?php previous_post_link('%link', '%title', TRUE); ?> </div>
+					<div class="nav-next"><?php next_post_link('%link', '%title', TRUE); ?> </div>
 				</div><!-- #nav-below -->
 
 				<?php comments_template( '', true ); ?>
